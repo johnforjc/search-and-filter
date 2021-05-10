634a6745cf90c1dbd5a8ce8fe93db3939a34dcbe
johnforjc <animeloverz072@gmail.com>
2021-05-10 13:50:56 +0700

    pagination done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import { useEffect, useState } from "react";
+import ReactPaginate from "react-paginate";
 import Filter from "./component/Filter";
 import Search from "./component/Search";
 import Books from "./component/Books";
 
 function App() {
   const [books, setbooks] = useState([]);
+  const [offset, setOffset] = useState(0);
+  const [perPage] = useState(10);
+  const [pageCount, setPageCount] = useState(0);
+
   const getData = () => {
     fetch("book.json", {
       headers: {
@@ -17,19 +22,39 @@ function App() {
         return response.json();
       })
       .then(function (myJson) {
-        setbooks(myJson);
+        const slice = myJson.slice(offset, offset + perPage);
+        setbooks(slice);
+        setPageCount(Math.ceil(myJson.length / perPage));
       });
   };
 
   useEffect(() => {
     getData();
-  }, []);
+  }, [offset]);
+
+  const handlePageClick = (e) => {
+    const selectedPage = e.selected;
+    setOffset(selectedPage + 1);
+  };
 
   return (
     <div id="myApp" className="container p-5 my-5">
       <Filter></Filter>
       <Search></Search>
       <Books books={books}></Books>
+      <ReactPaginate
+        previousLabel={"prev"}
+        nextLabel={"next"}
+        breakLabel={"..."}
+        breakClassName={"break-me"}
+        pageCount={pageCount}
+        marginPagesDisplayed={2}
+        pageRangeDisplayed={5}
+        onPageChange={handlePageClick}
+        containerClassName={"pagination"}
+        subContainerClassName={"pages pagination"}
+        activeClassName={"active"}
+      />
     </div>
   );
 }
--- src/component/Book.js
+++ src/component/Book.js
@@ -4,20 +4,28 @@ const Book = ({ book }) => {
   return (
     <a href={book.link}>
       <div className="row book-container mb-4 p-4">
-        <div className="col-4">
-          <img src="" alt="" />
+        <div className="col-sm-4">
+          <img className="book-img" src={book.imageLink} alt="" />
         </div>
-        <div className="col-8">
+        <div className="col-sm-8">
           <div className="row">
-            <h3>{book.title}</h3>
+            <h3 className="mb-3">{book.title}</h3>
           </div>
           <div className="row">
-            <div className="col fs-6">Author: {book.author}</div>
-            <div className="col-3 fs-6">Year: {book.year}</div>
+            <div className="col-5 fs-6">Author</div>
+            <div className="col fs-6">: {book.author}</div>
           </div>
           <div className="row">
-            <div className="col fs-6">Language: {book.language}</div>
-            <div className="col-3 fs-6">Pages: {book.pages}</div>
+            <div className="col-5 fs-6">Language</div>
+            <div className="col fs-6">: {book.language}</div>
+          </div>
+          <div className="row">
+            <div className="col-5 fs-6">Year</div>
+            <div className="col fs-6">: {book.year}</div>
+          </div>
+          <div className="row">
+            <div className="col-5 fs-6">Number of page</div>
+            <div className="col fs-6">: {book.pages}</div>
           </div>
         </div>
       </div>
